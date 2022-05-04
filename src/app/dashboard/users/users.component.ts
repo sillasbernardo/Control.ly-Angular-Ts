c1198cb3ee06d43e874db475ae68c61742b5dbd5
sillasbernard <syllasbernardo@gmail.com>
2022-05-04 15:39:16 -0300

    commit

--- src/app/dashboard/users/users.component.ts
+++ src/app/dashboard/users/users.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { faPenToSquare, faUser, faUserXmark } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
     selector: 'app-users',
@@ -7,4 +9,28 @@ import { Component } from "@angular/core";
 })
 
 export class UsersComponent{
+
+    faEditUser = faPenToSquare;
+    faDeleteUser = faUserXmark;
+
+    users = [
+        {
+            id: 1,
+            name: "Sillas Bernardo",
+            ldap: "sillas.bernardo",
+            department: "Coordenador I"
+        },
+        {
+            id: 2,
+            name: "Kenji Chikui",
+            ldap: "kenji.chikui",
+            department: "Estagiário"
+        },
+        {
+            id: 3,
+            name: "João Vitor",
+            ldap: "joao.vitor",
+            department: "Estagiário"
+        }
+    ];
 }
